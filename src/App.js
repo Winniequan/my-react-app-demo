f297c633cc1a72544ea3b2bd94218925a633dfd2
Ying Quan <quanying26@hotmail.com>
2023-08-17 13:08:16 -0500

    add a p

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div>
       <h1>my first react app.</h1>
+      <p>This is my first app deployed on netify and I am so happy that it works!</p>
     </div>
   );
 }
